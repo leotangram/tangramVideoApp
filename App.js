3b15429fdd2ac345ded549dd1eadafdccee664b3
Leonardo Omaña <leonardo.omana@imagineapps.co>
2018-10-13 17:43:59 -0500

    Support for iPhone X in React Native: Creation of the Header

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import {
   Text
 } from 'react-native';
 import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <Home>
-        <Text>header</Text>
+        <Header />
         <Text>buscador</Text>
         <Text>categorias</Text>
         <Text>sugerencias</Text>
