c0d868508800fdb8958676ffbd6005bccfacc937
Leonardo Omaña <leonardo.omana@imagineapps.co>
2018-10-15 18:24:19 -0500

    Suggestion component

--- src/videos/containers/suggestion-list.js
+++ src/videos/containers/suggestion-list.js
@@ -4,11 +4,17 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Layout from '../components/suggestion-list-layout';
 import Empty from '../components/empty';
 import Separator from '../components/vertical-separator';
+import Suggestion from '../components/suggestion';
 
 // create a component
 class SuggestionList extends Component {
   renderEmpty = () => <Empty text="No hay sugerencias :(" />
   itemSeparator = () => <Separator />
+  renderItem = ({ item }) => {
+    return (
+      <Suggestion {...item} />
+    )
+  }
   render() {
     const list = [
       {
@@ -28,7 +34,7 @@ class SuggestionList extends Component {
           data={list}
           ListEmptyComponent={this.renderEmpty}
           ItemSeparatorComponent={this.itemSeparator}
-          renderItem={({ item }) => <Text>{item.title}</Text>}
+          renderItem={this.renderItem}
         />
       </Layout>
     )
