e9481936454e759f8385a2e52f0a778ecab33c7b
Leonardo Omaña <leonardo.omana@imagineapps.co>
2018-10-15 18:54:08 -0500

    Making requests to a server | fetch

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Text
 } from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
+import Api from './utils/api';
 
 type Props = {};
 export default class App extends Component<Props> {
+  async componentDidMount() {
+    const movies = await Api.getSuggestion(10);
+    console.log(movies);
+  }
   render() {
     return (
       <Home>
@@ -15,7 +20,7 @@ export default class App extends Component<Props> {
         <Text>buscador</Text>
         <Text>categorias</Text>
         <SuggestionList />
-      </Home>      
+      </Home>
     );
   }
 }
